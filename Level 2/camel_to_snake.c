43ab69d0e51522a236c57214028a43824856bc58
Tatiana Sofia Gomes Lima <tgomes-l@c2s14d80.42wolfsburg.de>
2023-01-26 15:31:59 +0100

    level 1 and 2

---
+++ Level 2/camel_to_snake.c
@@ -0,0 +1,49 @@
+/*Assignment name  : camel_to_snake
+Expected files   : camel_to_snake.c
+Allowed functions: malloc, realloc, write
+--------------------------------------------------------------------------------
+
+Write a program that takes a single string in lowerCamelCase format
+and converts it into a string in snake_case format.
+
+A lowerCamelCase string is a string where each word begins with a capital letter
+except for the first one.
+
+A snake_case string is a string where each word is in lower case, separated by
+an underscore "_".
+
+Examples:
+$>./camel_to_snake "hereIsACamelCaseWord"
+here_is_a_camel_case_word
+$>./camel_to_snake "helloWorld" | cat -e
+hello_world$
+$>./camel_to_snake | cat -e
+$*/
+
+static int	ft_isuppercase(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = i - 1;
+	if(argc == 2)
+	{
+		while(argv[1][i])
+		{
+			if(argv[1][i] == ft_isuppercase)
+			{
+				
+			}
+		i++;
+		j++;
+		}
+	}
+}
