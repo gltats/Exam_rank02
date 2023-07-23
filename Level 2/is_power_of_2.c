d471bf5f13e035d607935380a86c1b4090308b7d
gltats <tatianasofiagomes831@gmail.com>
2023-07-23 17:07:14 -0400

    tests updated

--- Level 2/is_power_of_2.c
+++ Level 2/is_power_of_2.c
@@ -11,3 +11,25 @@ This function returns 1 if the given number is a power of 2, otherwise it return
 Your function must be declared as follows:
 
 int	    is_power_of_2(unsigned int n);*/
+
+#include <stdio.h>
+
+int is_power_of_2(unsigned int n)
+{
+    return (n != 0 && (n & (n - 1)) == 0);
+}
+
+int main()
+{
+    // Test cases
+    unsigned int numbers[] = {0, 1, 2, 3, 4, 8, 16, 32, 64, 128, 255, 256, 1024};
+
+    printf("Testing is_power_of_2:\n");
+
+    for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++)
+    {
+        printf("%u is %s a power of 2\n", numbers[i], is_power_of_2(numbers[i]) ? "" : "not");
+    }
+
+    return 0;
+}
--- Level_1/search_remplace.c
+++ Level_1/search_remplace.c
@@ -25,3 +25,25 @@ $>./search_and_replace "ZoZ eT Dovid oiME le METol." "o" "a" | cat -e
 ZaZ eT David aiME le METal.$
 $>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
 eNcOre Un ExEmPle Pas Facile a Ecrire $*/
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+
+    if (argc == 4 && !argv[2][1] && !argv[3][1])
+    {
+        while (argv[1][i]!='\0')
+        {
+            if (argv[1][i]==argv[2][0])
+            {
+                argv[1][i] = argv[3][0];
+            }
+            write (1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write (1, "\n", 1);
+    return(0);
+}
